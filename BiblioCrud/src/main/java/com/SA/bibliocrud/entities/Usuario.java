47930625a7beac510bf1aa05a4add086d20e5aed
Luiz Eduardo Vieira de Freitas <62312229+luizevf@users.noreply.github.com>
2023-11-14 16:34:45 -0300

    Add files via upload

--- BiblioCrud/src/main/java/com/SA/bibliocrud/entities/Usuario.java
+++ BiblioCrud/src/main/java/com/SA/bibliocrud/entities/Usuario.java
@@ -1,20 +1,42 @@
 package com.SA.bibliocrud.entities;
 
-import java.lang.annotation.Inherited;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "Usuarios")
-public class Usuario(){
 
-@Id
-@GeneratedValue
-@Column(name="UserId")
-  private long UserId;
+public class Usuario {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "UserID")
+  private Long UserId;
+
+  @Column(name = "Nome")
   private String Nome;
+
+  @Column(Name = "Sobrenome")
+  private String Sobrenome;
+
+  @Column(name = "Email")
   private String Email;
-  private String Password;
+
+  @Column(name = "Senha")
+  private String Senha;
 
 }
